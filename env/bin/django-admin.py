3b54212075d090b2ce77d5a14a0ae932e28ac2d0
Mayank Kapoor <mayankkapoormail@gmail.com>
2014-02-01 14:04:32 -0800

    Initial commit of myproject

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/mayankkapoor/Dropbox/GitHub/2014-02-01_FindMyCar_Server/env/bin/python3.3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
