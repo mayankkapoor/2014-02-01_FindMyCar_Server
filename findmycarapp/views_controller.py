96b42e04659aa1904e712064f3ff1f8fc0bed547
Mayank Kapoor <mayankkapoormail@gmail.com>
2014-02-02 13:35:57 -0800

    Simple twilio send sms working

--- findmycarapp/views_controller.py
+++ findmycarapp/views_controller.py
@@ -1,5 +1,19 @@
 from django.http import HttpResponse
+from twilio.rest import TwilioRestClient
 
 # Create your views here.
 def index(request):
 	return HttpResponse("Hello World! You're at the index page.")
+
+def send_SMS(request):
+	sms_to = request.GET['sms_to']
+	sms_body = request.GET['sms_body']
+	sms_from = "+17818503507"
+	ACCOUNT_SID = "AC2b9daf9a366930d914fa56be530a5f0e"
+	AUTH_TOKEN = "434bd7072378dd99ab63892c04182f94"
+
+	client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
+	message = client.messages.create(to=sms_to, from_=sms_from, body=sms_body)
+
+	return HttpResponse("SMS sent. Check phone.")
+
